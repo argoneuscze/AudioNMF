638daab89515c55e212c157b8511f066f75396b5
argoneus <argoneuscze@gmail.com>
2018-10-24 11:59:06 +0200

    added a debug command for easy debugging

--- audionmf/cli.py
+++ audionmf/cli.py
@@ -16,20 +16,7 @@ def get_output_handle(input_filename, filetype):
     return open(target_name, 'wb')
 
 
-@click.group()
-def cli():
-    pass
-
-
-@cli.command()
-@click.argument('input_file', type=click.File('rb'))
-@click.argument('output_file', type=click.File('wb'), required=False)
-def compress(input_file, output_file):
-    filename = input_file.name
-    filetype = get_filename_ext(filename)[1].lower()[1:]
-    if output_file is None:
-        output_file = get_output_handle(filename, 'anmf')
-
+def compress(input_file, output_file, filetype):
     audio = AudioFile.read_file(input_file, filetype)
 
     if audio is None:
@@ -37,20 +24,50 @@ def compress(input_file, output_file):
     else:
         audio.compress(output_file)
 
-    input_file.close()
-    output_file.close()
 
-
-@cli.command()
-@click.argument('input_file', type=click.File('rb'))
-@click.argument('output_file', type=click.File('wb'), required=False)
-@click.option('-t', '--filetype', type=click.Choice(['wav', 'flac']), default='wav')
 def decompress(input_file, output_file, filetype):
-    if output_file is None:
-        output_file = get_output_handle(input_file.name, filetype)
-
     audio = AudioFileCompressed.read_file(input_file)
     audio.decompress(output_file, filetype)
 
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command(name='compress')
+@click.argument('input_file', type=click.File('rb'))
+@click.argument('output_file', type=click.File('wb'), required=False)
+def compress_command(input_file, output_file):
+    filename = input_file.name
+    filetype = get_filename_ext(filename)[1].lower()[1:]
+    if output_file is None:
+        output_file = get_output_handle(filename, 'anmf')
+
+    compress(input_file, output_file, filetype)
+
     input_file.close()
     output_file.close()
+
+
+@cli.command(name='decompress')
+@click.argument('input_file', type=click.File('rb'))
+@click.argument('output_file', type=click.File('wb'), required=False)
+@click.option('-t', '--filetype', type=click.Choice(['wav', 'flac']), default='wav')
+def decompress_command(input_file, output_file, filetype):
+    if output_file is None:
+        output_file = get_output_handle(input_file.name, filetype)
+
+    decompress(input_file, output_file, filetype)
+
+    input_file.close()
+    output_file.close()
+
+
+@cli.command(name='debug')
+@click.argument('input_file', type=click.File('rb'))
+def debug_command(input_file):
+    with open('debug.anmf', 'wb') as anmf_file:
+        compress(input_file, anmf_file, 'wav')
+    with open('debug.anmf', 'rb') as anmf_file, open('debug.wav', 'wb') as wav_file:
+        decompress(anmf_file, wav_file, 'wav')
