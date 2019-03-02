fe81617b8c473d8f793e78bc38821a7361cb320c
argoneus <argoneuscze@gmail.com>
2019-03-02 15:50:24 +0100

    added a script to evaluate quality using GstPEAQ

---
+++ scripts/eval_quality.py
@@ -0,0 +1,45 @@
+import os
+import subprocess
+
+use_advanced = False
+
+# ===
+
+examples_dir = '../examples'
+output_dir = '../debug'
+
+comp_map = {
+    'r': 'RAW',
+    'f': 'FFT',
+    'm': 'MDCT',
+    's': 'STFT'
+}
+
+# ===
+
+peaq_flag = '--basic'
+if use_advanced:
+    peaq_flag = '--advanced'
+
+sample_filenames = list()
+
+for file in os.listdir(examples_dir):
+    if file.endswith('.wav'):
+        sample_filenames.append(os.path.splitext(file)[0])
+
+for fn in sample_filenames:
+    orig = os.path.join(examples_dir, '{}.wav'.format(fn))
+
+    print('== Testing {} =='.format(fn))
+
+    for comp in comp_map.keys():
+        c_file = os.path.join(output_dir, '{}_dec_anmf{}.wav'.format(fn, comp))
+        peaq_out = subprocess.check_output(['peaq', peaq_flag, orig, c_file]).decode('utf-8')
+
+        print(c_file)
+
+        spl = peaq_out.split()
+        odg = float(spl[3])
+        di = float(spl[6])
+
+        print('[{}] Objective Difference Grade: {}, Distortion Index: {}'.format(comp_map[comp], odg, di))
