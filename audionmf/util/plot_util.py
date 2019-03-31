751955bf7984f5f56c6d754513906abb14edfcfb
argoneus <argoneuscze@gmail.com>
2019-03-31 14:06:27 +0200

    updated plot function to allow for labels

--- audionmf/util/plot_util.py
+++ audionmf/util/plot_util.py
@@ -1,8 +1,15 @@
 import matplotlib.pyplot as plt
 
 
-def plot_signal(signal, filename):
+def plot_signal(signal, filename, title=None, x_label=None, y_label=None):
     fig, ax = plt.subplots()
+    if title:
+        ax.set_title(title)
+    if x_label:
+        ax.set_xlabel(x_label)
+    if y_label:
+        ax.set_ylabel(y_label)
     ax.plot(signal)
+    fig.tight_layout()
     fig.savefig(filename)
     plt.close(fig)
